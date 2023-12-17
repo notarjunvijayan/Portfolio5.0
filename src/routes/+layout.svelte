ca03e2eb257e2dff41f2f66dc0342ac58f3d5e8a
arjun <vijayanarjun4@gmail.com>
2023-12-17 09:38:44 +0530

    Initial COmmit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<nav>
+    <h2>AV</h2>
+    <button>Menu</button>
+</nav>
+<style>
+    nav{
+        display: flex;
+        border-bottom: 1px solid white;
+        background: rgba( 255, 255, 255, 0.25 );
+        backdrop-filter: blur(11px) saturate(122%);
+        -webkit-backdrop-filter: blur(8px) saturate(122%);
+        background-color: rgba(16, 16, 16, 0.62);
+        border-bottom: 1px solid rgba(255, 255, 255, 0.125);
+        color:white;
+        position: fixed;
+        width: 94dvw;
+        height: 10dvh;
+        align-items: center;
+        padding: 0 3dvw;
+        justify-content: space-between;
+    }
+    h2{
+        font-family: mono;
+        font-size: 2rem;
+    }
+</style>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="container">
+    <a href="/about">About</a>
+</div>
+
+
+<style>
+    .container{
+        background-color: #101010;
+        color:white;
+        padding-top: 10vh;
+        height: 100vh;
+    }
+</style>
